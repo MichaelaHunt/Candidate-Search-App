c87bc846536e5bfc2745dc6052b0ffa37244e285
Michaela Hunt <mhunt@theprovatogroup.com>
2024-11-21 17:21:25 -0500

    Table html completed

--- src/pages/SavedCandidates.tsx
+++ src/pages/SavedCandidates.tsx
@@ -1,9 +1,50 @@
+import { useState } from "react";
+import "./SavedCandidates.css";
+
 const SavedCandidates = () => {
+  let [candidates] = useState<Candidate[]>(getStorage);
+
+  function getStorage(): Candidate[] {
+    const data = localStorage.getItem('candidateList');
+    let storage: Candidate[] = JSON.parse(data!);
+    return storage;
+  }
+
   return (
     <>
       <h1>Potential Candidates</h1>
+      {candidates && (
+        <table>
+          <thead>
+            <tr>
+              <th>Image</th>
+              <th>Name</th>
+              <th>Location</th>
+              <th>Email</th>
+              <th>Company</th>
+              <th>Bio</th>
+              <th>Reject</th>
+            </tr>
+            {candidates.map((item) => (
+              <tr key={item.html_url}>
+                <td><img src={item.avatar_url ? item.avatar_url : "Not found."}></img></td>
+                <td>{item.name} ({item.login})</td>
+                <td>{item.location ? item.location : "Not found."}</td>
+                <td>{item.email ? item.email : "Not found."}</td>
+                <td>{item.company ? item.company : "Not found."}</td>
+                <td>{item.bio ? item.bio : "Not found."}</td>
+                <td><button>-</button></td>
+              </tr>
+            ))}
+          </thead>
+          <tbody>
+            {/* You can render table rows here */}
+          </tbody>
+        </table>
+      )}
     </>
   );
+
 };
 
 export default SavedCandidates;
