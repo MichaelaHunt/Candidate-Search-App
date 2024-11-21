0ea7332f2301325cb604a28addb469b4a0e205cc
Michaela Hunt <mhunt@theprovatogroup.com>
2024-11-20 23:24:47 -0500

    Nav component finished; Candidate interface started

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,12 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+interface Candidate {
+    username: string;//this is the login field in the response
+    image: string;//avatar_url
+    link: string;//html_url
+    location?: string;
+    email?: string;
+    bio?: string;
+    name?: string;
+    company?: string;
+}
+//name, username, location, avatar, email, html url, company
