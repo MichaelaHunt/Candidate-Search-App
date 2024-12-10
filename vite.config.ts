1ea4d9d0c187c9727a804a9628265c53e5085b81
Michaela Hunt <micapplehunt@gmail.com>
2024-12-10 16:01:17 -0500

    config change of port

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  envDir: './env',
+  server: {
+    port: 3000
+  },
+  envDir: './environment',
   plugins: [react()],
 });
